5d939ddefb6f7e778293f3a38dd52bf1ca1d4d76
Matt Davis <jiffyclub@gmail.com>
2015-10-16 16:25:22 -0700

    starting a Python wrapper for the CFFI C extension

---
+++ pandana/_pyaccess.py
@@ -0,0 +1,26 @@
+from ._accesswrap import ffi, lib
+
+
+def create_graphs(num):
+    lib.create_graphs(num)
+
+
+def create_graph(id, nodes, edges, impedance_names, twoway):
+    numnodes = len(nodes)
+    numedges = len(edges)
+    numimpedances = len(impedance_names)
+
+    nodeids = nodes.index.values.astype('int32')
+    nodeids_ptr = ffi.cast('int *', nodeids.ctypes.data)
+    nodexy = nodes.as_matrix().astype('float32')
+    nodexy_ptr = ffi.cast('float *', nodexy.ctypes.data)
+    edges_arr = edges[['from', 'to']].as_matrix().astype('int32')
+    edges_ptr = ffi.cast('int *', edges_arr.ctypes.data)
+    edge_weights = (
+        edges[impedance_names].transpose().as_matrix().astype('float32'))
+    edge_weights_ptr = ffi.cast('float *', edge_weights.ctypes.data)
+    twoway = ffi.cast('int', twoway)
+
+    lib.create_graph(
+        id, numnodes, nodeids_ptr, nodexy_ptr, numedges, numimpedances,
+        edges_ptr, edge_weights_ptr, twoway)
